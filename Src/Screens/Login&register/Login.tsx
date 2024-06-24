2bb6cd18c098b0d59c68fa56a08eccd2af2e7919
yousef khaled <yousefkhaled567@gmail.com>
2024-06-24 13:18:04 +0300

    show pass

--- Src/Screens/Login&register/Login.tsx
+++ Src/Screens/Login&register/Login.tsx
@@ -1,22 +1,47 @@
 import {  Text, TextInput, TouchableOpacity, View } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { NavigationProp, useNavigation } from '@react-navigation/native';
+import { StyleSheet } from "react-native"
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 type LoginProps = {
   navigation: NavigationProp<any>;
 };
 
+
+
+
 const Login = ({navigation}: LoginProps) => {
+    const [password, setPassword] = useState('');
+const [secureTextEntry, setSecureTextEntry] = useState(true); // State to manage password visibility
+
+const toggleSecureEntry = () => {
+  setSecureTextEntry(!secureTextEntry);
+};
+
   return (
     <View style={styles.container}>
       <Text style={styles.title} >Login</Text>
       <TextInput style={styles.input} placeholder="Email" />
-      <TextInput
-        style={styles.input}
-        placeholder="Password"
-        secureTextEntry
-      />
+  
+  
+ 
+      <TextInput 
+          style={styles.input}
+          placeholder="Password"
+          secureTextEntry={secureTextEntry} // Toggle secure text entry based on state
+          value={password}
+          onChangeText={setPassword}
+          
+        >
+            
+        </TextInput>
+        <TouchableOpacity onPress={toggleSecureEntry} >
+       <Text style={{ textAlign: 'center', color:"white"}}> {secureTextEntry ? 'Show Password' : 'Hide Password'}</Text>
+      </TouchableOpacity>
+                 
+
       <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('HomeScreen')}>
         <Text style={styles.buttonText}>Login</Text>
       </TouchableOpacity>
@@ -88,5 +113,4 @@ export default Login
 
 
 
-import { StyleSheet } from "react-native"
 

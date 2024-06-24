a0114b93057226bd0fa20b2ffeef7b1cd5f38292
yousef khaled <yousefkhaled567@gmail.com>
2024-06-24 13:36:45 +0300

    styles alone

--- Src/Screens/Login&register/Login.tsx
+++ Src/Screens/Login&register/Login.tsx
@@ -2,6 +2,7 @@ import {  Text, TextInput, TouchableOpacity, View } from 'react-native'
 import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { NavigationProp, useNavigation } from '@react-navigation/native';
+import { styles } from './styles';
 import { StyleSheet } from "react-native"
 import Icon from 'react-native-vector-icons/FontAwesome';
 
@@ -55,60 +56,7 @@ const toggleSecureEntry = () => {
     </View>
   )
 }
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: 'black',
-      paddingHorizontal: 20,
-    },
-    title: {
-        color: '#ffff',
-      fontSize: 32,
-      fontWeight: 'bold',
-      marginBottom: 40,
-    },
-    input: {
-      width: '100%',
-      height: 50,
-      borderWidth: 1,
-      borderColor: '#ccc',
-      borderRadius: 5,
-      paddingHorizontal: 10,
-      marginBottom: 20,
-      backgroundColor: '#fff',
-    },
-    button: {
-      width: '100%',
-      height: 50,
-      backgroundColor: '#007BFF',
-      justifyContent: 'center',
-      alignItems: 'center',
-      borderRadius: 5,
-      marginBottom: 20,
-    },
-    buttonText: {
-      color: '#fff',
-      fontSize: 18,
-      fontWeight: 'bold',
-    },
-    forgotPassword: {
-      color: '#007BFF',
-      marginBottom: 20,
-    },
-    signupContainer: {
-      flexDirection: 'row',
-      alignItems: 'center',
-    },
-    signupText: {
-      color: '#888',
-    },
-    signupButton: {
-      color: '#007BFF',
-      marginLeft: 5,
-    },
-  })
+
 export default Login
 
 
